62c6e3291d526a7f3d8aac39ed2a293a787ef177
ChristineMuchiri <122861795+ChristineMuchiri@users.noreply.github.com>
2023-10-24 11:35:38 +0300

    lamp_server.sh

--- Lamp-Server/lamp_server.sh
+++ Lamp-Server/lamp_server.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-echo "This scripts sets up a lamp server on an Ubuntu VM"
+echo "This scripts sets up a lamp server on an Amazon Linux 2"
 
-sudo apt-get update
-sudo apt-get install apache2 #installing Apache
-sudo apt-get install mysql-server #installing MySQL
-sudo mysql_secure_installation #Secure MySQL installation
-sudo apt-get install php libapache2-mod-php php-mysql  #installing PHP and required extensions
+sudo yum update -y
+sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
 
+# Checking version of Amazon Linux
+cat /etc/system-release
+
+sudo yum install -y httpd mariadb-server
+
+# Start Apache web server
+sudo systemctl start httpd
+sudo systemctl is-enabled httpd
+
+# Set file permissions
+sudo usermod -a -G apache ec2-user
+
+# Change group ownership
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
+
+# SECURE DATABASE SERVER
+sudo systemctl start mariadb
+sudo mysql_secure_installation
